af7dd92a39096450a4f2cc47e3bba9993c1ae613
kahvelikodlama <kahvelikodlama@gmail.com>
2022-05-08 12:24:52 +0300

    Proje dosyası ekle.

---
+++ MAUI- Calculator App/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI__Calculator_App;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MAUI- Calculator App/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MAUI__Calculator_App;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	void Clear(object sender, EventArgs e)
+    {
+
+    }
+}
+
