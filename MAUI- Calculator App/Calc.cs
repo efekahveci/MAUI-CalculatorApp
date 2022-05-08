d170a35ff28c4045bb867c73f392588ed36b802e
kahvelikodlama <kahvelikodlama@gmail.com>
2022-05-08 23:31:42 +0300

    single digit complated

---
+++ MAUI- Calculator App/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI__Calculator_App
+{
+    internal static class Calc
+    {
+        internal static double Do(double d1,double d2,string ch)
+        {
+      
+            switch (ch)
+            {
+                case "/":
+                    return d1 / d2;
+                case "X":
+                    return d1 * d2;
+                case "+":
+                    return d1 + d2;
+                case "-":
+                    return d1 - d2;
+                default:
+                    break;
+
+            }
+
+            return 0;
+          
+        }
+    }
+}
--- MAUI- Calculator App/MainPage.xaml.cs
+++ MAUI- Calculator App/MainPage.xaml.cs
@@ -2,16 +2,67 @@
 
 public partial class MainPage : ContentPage
 {
-
-
+	private int current = 1;
+	private string val1, val2;
+	string omath, press;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
 	void Clear(object sender, EventArgs e)
-    {
+	{
+		val1 = "0";
+		val2 = "0";
+		current = 1;
+		this.result.Text = "0";
+	}
+	void AddZero(object sender, EventArgs e)
+	{
+		Button button = sender as Button;
+		press = press + button.Text;
 
-    }
+		if (val1 != "0")
+			val1 = val1 + button.Text;
+		else
+			val2 = val2 + button.Text;
+
+		this.result.Text = press;
+	}
+	void NumberSelect(object sender, EventArgs e)
+	{
+
+		Button button = sender as Button;
+
+
+		press = button.Text;
+		this.result.Text = press;
+
+
+
+		if (current == 1)
+		{
+			val1 = press;
+			current++;
+		}
+		else
+		{
+			val2 = press;
+			current--;
+		}
+
+	}
+
+	void OperatorSelect(object sender, EventArgs e)
+	{
+		Button button = sender as Button;
+
+		omath = button.Text;
+	}
+
+	void Calculate(object sender, EventArgs e)
+	{
+		this.result.Text = Calc.Do(Convert.ToDouble(val1), Convert.ToDouble(val2), omath).ToString();
+	}
 }
 
